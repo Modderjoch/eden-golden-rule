b162a35a87450c6f41a819be6ee34c0af3bba219
Oebe Rademaker <499574@student.saxion.nl>
2024-03-12 11:54:15 +0100

    Added simple 'trash' props and a random spawner

--- Eden/Assets/InteractableSpawner.cs
+++ Eden/Assets/InteractableSpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class InteractableSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject prefabToSpawn;
+    [SerializeField] private List<GameObject> prefabToSpawnList;
     [SerializeField] private int numberOfObjectsToSpawn = 3;
 
     [SerializeField] private Transform parentToSpawnTo;
@@ -18,15 +18,18 @@ public class InteractableSpawner : MonoBehaviour
     {
         for (int i = 0; i < numberOfObjectsToSpawn; i++)
         {
+            int randomPrefabIndex = Random.Range(0, prefabToSpawnList.Count);
+            GameObject prefabToSpawn = prefabToSpawnList[randomPrefabIndex];
+
                 Vector3 randomPosition = new Vector3(
                 Random.Range(transform.position.x - transform.localScale.x / 2, transform.position.x + transform.localScale.x / 2),
-                Random.Range(transform.position.y - transform.localScale.y / 2, transform.position.y + transform.localScale.y / 2),
+                Random.Range(transform.position.y + 0.025f, transform.position.y + 0.025f),
                 Random.Range(transform.position.z - transform.localScale.z / 2, transform.position.z + transform.localScale.z / 2)
             );
 
             Quaternion randomRotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
 
-            GameObject instantiatedPrefab = Instantiate(prefabToSpawn, randomPosition, randomRotation, parentToSpawnTo);
+            Instantiate(prefabToSpawn, randomPosition, randomRotation, parentToSpawnTo);
         }
     }
 }
