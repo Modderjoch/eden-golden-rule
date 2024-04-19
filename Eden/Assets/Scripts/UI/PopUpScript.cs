09ea2dc5d8d2c38ac75ea54dec1c54e663e94cee
Joris Kok <Koksiroj@gmail.com>
2024-04-19 14:50:16 +0200

    Pop-up Script update
    Call with `PopUpEntry("text", seconds);`

--- Eden/Assets/Scripts/UI/PopUpScript.cs
+++ Eden/Assets/Scripts/UI/PopUpScript.cs
@@ -7,13 +7,27 @@ using UnityEngine.UI;
 
 public class PopUpScript : MonoBehaviour
 {
+    private Animator animator;
+
     [SerializeField] private TMP_Text popUpMsg;
     public string popUpText = "gamer";
 
-    private void OnEnable()
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
         popUpMsg.text = popUpText;
     }
 
+    public void PopUpEntry(string text, int x)
+    {
+        popUpMsg.text = text;
+        animator.SetTrigger("entry");
+        StartCoroutine(PopUpClose(x));
+    }
+    
+    IEnumerator PopUpClose(int x)
+    {
+        yield return new WaitForSeconds(x);
+        animator.SetTrigger("close");
+    }
 }
